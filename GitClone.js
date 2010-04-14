fd3cf41f1e1b882cc45ebbf4c0ba7d8f4383a8f8
Alan Knowles <alan@wideboy.(none)>
2010-04-14 22:50:34 +0800

    GitClone.js

--- GitClone.js
+++ GitClone.js
@@ -1,10 +1,12 @@
 ///<script type="text/javascript">
 
 Gtk      = imports.gi.Gtk;
+Gtk.init(null,null);
 
 var builder = new Gtk.Builder();
 builder.add_from_file(__script_path__+'/gitlive.builder');
 var win = builder.get_object('clone_repo');
 win.show();
+Gtk.main();
 
  
