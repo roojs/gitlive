6eef1da8b955f63475cdac21813eacda524b46ba
alan <alan@alanfast.akbkhome.com>
2010-04-15 19:19:56 +0800

    XObject.js

--- XObject.js
+++ XObject.js
@@ -20,7 +20,7 @@ function XObject (cfg) {
     // first apply cfg if set.
     
     print("new xobj?"  + XObject.keys(cfg).join(','));
-    print(cfg);
+    //print(cfg);
     o =  {};
     
     cfg.items = cfg.items || [];
