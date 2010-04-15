e266aba04b6b5e71b91fdc7b2fce8bdbe82e58e7
Alan Knowles <alan@wideboy.(none)>
2010-04-15 22:24:23 +0800

    StatusIcon.js

--- StatusIcon.js
+++ StatusIcon.js
@@ -37,7 +37,7 @@ StatusIcon  = new XObject({
             
             menu.el.popup(null, null, null, event, event_time);
             var g = this.el.get_geometry();
-            menu.el.set_uposition(pg.area.x,pg.area.y);
+            menu.el.set_uposition(g.area.x,g.area.y);
             
             
             //menu.el.popup(null, null , null, null,  event, event_time);
