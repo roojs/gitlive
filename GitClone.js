f46586c67391c7b36204850c3d1706f789755832
Alan Knowles <alan@wideboy.(none)>
2010-04-19 07:21:25 +0800

    GitClone.js

--- GitClone.js
+++ GitClone.js
@@ -2,6 +2,7 @@
 
 Gtk      = imports.gi.Gtk;
 
+
 imports.gtkbuilder;
 
 Gtk.init(null,null);
@@ -9,12 +10,14 @@ Gtk.init(null,null);
 var builder = new Gtk.Builder();
 builder.add_from_file(__script_path__+'/gitlive.builder');
 var win = builder.get_object('clone_repo');
-builder.get_object('ok').signal.clicked.connect( function() {
+builder.connect_signals({
+    on_ok :  function() {
         win.hide();
-});
-builder.get_object('cancel').signal.clicked.connect( function() {
+    }, 
+    on_cancel : function() {
         win.hide();
-});
+    }
+);
 
 var hosts = builder.get_object('hosts');
 hosts.get_model().clear();
