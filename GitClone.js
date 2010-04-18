3961a958b91c44ecad35773b17239c0307387683
Alan Knowles <alan@wideboy.(none)>
2010-04-19 07:36:15 +0800

    GitClone.js

--- GitClone.js
+++ GitClone.js
@@ -21,7 +21,7 @@ builder.connect_signals({
 
 Gtk.GtkListStore.prototype.setValue(r, c ,v)
 {
-    var tp = new Gtk.TreePath.from_string ('' + r) :
+    var tp = new Gtk.TreePath.from_string('' + r) ;
     var citer = new Gtk.TreeIter();
     if (!get_iter (citer, tp)) {
         this.append(citer);
