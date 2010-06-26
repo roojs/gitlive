fa5ae3b9d43824cc4a24e79d668274e9759fd600
Alan Knowles <alan@akkbhome.com>
2010-06-26 11:04:32 +0800

    WindowLog.js

--- WindowLog.js
+++ WindowLog.js
@@ -5,7 +5,11 @@
  * mouse:
  * xdotool getmouselocation
  *
- * 
+ * xwit -property WM_NAME -print 
+ * 0x3a13d6c: x=1 y=25 w=1109 h=747 d=32 'root@wideboy: /home/alan'
+ * root@wideboy:/home/alan# xwit -property WM_CLASS -print 
+ * 0x3a13d6c: x=1 y=25 w=1109 h=747 d=32 'gnome-terminal'
+ * root@wideboy:/home/alan# 
  */
 
 
