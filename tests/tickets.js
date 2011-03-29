605614bed9758c14b2c3ab71b522aebcf5b67d43
Alan Knowles <alan@akbkhome.com>
2011-03-29 17:52:48 +0800

    tests/tickets.js

--- tests/tickets.js
+++ tests/tickets.js
@@ -13,14 +13,14 @@ Tickets = {
         // very basic parsing - only support single line version..
         var lines = File.read(Lib.get_home_dir() + '.netrc').split(/\n/);
         var t = this;
-        lines.forEach(function(l)) {
+        lines.forEach(function(l)  {
             if (!l.match(/^machine/)) {
                 return;
             }
             var ar = l.split(/\s+/);
             // assume machine XXX login XXX password XXXX
             t.machines[ar[1]] = { login : ar[3], password: ar[4]}
-        }
+        });
         
         
     }
