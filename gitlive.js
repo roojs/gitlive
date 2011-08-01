42b35dc745337175cab50814e9682011614903d7
Alan Knowles <alan@akbkhome.com>
2011-08-01 19:05:44 +0800

    gitlive.js

--- gitlive.js
+++ gitlive.js
@@ -325,7 +325,6 @@ var monitor = new Monitor({
         
         
         
-        
         var add_it = false;
         if (typeof(this.just_created[src.path]) !='undefined') {
             delete this.just_created[src.path];
