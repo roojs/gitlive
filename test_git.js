e29fa161c103ead9a628fdb136e8cb2b51bf71d7
Alan Knowles <alan@akbkhome.com>
2011-09-10 16:22:54 +0800

    new files

---
+++ test_git.js
@@ -0,0 +1,19 @@
+var Gtk = imports.gi.Gtk;
+
+Gtk.init(Seed.argv);
+
+var Git = imports.Git.Git;
+var git = new Git('/home/alan/gitlive/roojs1');
+git.async = true;
+var out = git.run(
+{ 'no-pager' : true },  
+   'log',
+   'github..master',
+   { 'no-color' : true },
+   { 'raw' : true },
+   { 'numstat' : true },
+   { 'date' : 'rfc' }  
+);
+print(out);
+
+Gtk.main();
