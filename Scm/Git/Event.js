c70d8f65348bcf772c89e6965cec02aba0e71efa
Alan Knowles <alan@akbkhome.com>
2011-09-10 20:48:13 +0800

    Scm/Git/Event.js

--- Scm/Git/Event.js
+++ Scm/Git/Event.js
@@ -4,7 +4,7 @@ var XObject = imports.XObject.XObject;
 Event = XObject.define( 
     function (cfg) {
         // do nothing?? universal ctr ?
-        Roo.apply(this,cfg);
+        XObject.apply(this,cfg);
         if (cfg.commit) {
             this.parseCommit();
         }
