506fadcf9ef36846521a2f8f8f27fcfda0e1de5b
Alan Knowles <alan@akbkhome.com>
2011-09-11 17:39:10 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -246,7 +246,7 @@ Merger=new XObject({
                                                  this.el.modify_font(description);
                                             
                                                  this.selection = this.el.get_selection();
-                                                  this.selection.set_mode( Gtk.SelectionMode.SINGLE);
+                                                  this.selection.set_mode( Gtk.SelectionMode.MULTIPLE);
                                                 var _this = this;
                                                 /*
                                              780                                                                     
