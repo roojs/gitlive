458ebf1078a2e9ba20b326437cf62d7be440bad7
Alan Knowles <alan@akbkhome.com>
2011-09-12 07:18:35 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -429,7 +429,7 @@ Merger=new XObject({
                                                // remove..
                                                 var s  = "document.body.textContent='';var pre  = document.createElement('pre'); document.body.appendChild(pre);";
                                                 s += "pre.textContent =  " +
-                                                         JSON.stringify(repo.lastCmd) + '+ "\n" + ' + 
+                                                         JSON.stringify(repo.lastCmd + "\n") + '+  ' + 
                                                        JSON.stringify(diff) + ";";
                                                 print(s);
                                                     
