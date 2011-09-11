9c459f51890cb0de0b507e7088b44933f77b12bb
Alan Knowles <alan@akbkhome.com>
2011-09-11 17:01:02 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -81,20 +81,15 @@ Merger=new XObject({
                             listeners : {
                                 clicked : function (self) {
                                     
-                                    print("running git");
-                                    var Git = imports.Git.Git;
-                                    var git = new Git('/home/alan/gitlive/roojs1');
-                                    git.async = true;
-                                    var out = git.run(
-                                       { 'no-pager' : true },  
-                                        'log',
-                                        'github..master',
-                                        { 'no-color' : true },
-                                        { 'raw' : true },
-                                        { 'numstat' : true },
-                                        { 'date' : 'rfc' }  
-                                       
-                                    );
+                                    
+                                    var Repo = imports.Scm.Git.Repo.Repo;
+                                
+                                    
+                                    var repo = new Repo({ repopath : '/home/alan/gitlive/roojs1' });
+                                 
+                                 
+                                var hist = repo.dayTree('/', false, 'rev', 'github..master');
+                                 
                                     print(out);
                                         
                                     
