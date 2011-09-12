2e7752653251e953fcde3b6e90f2ff575c0de90d
Alan Knowles <alan@akbkhome.com>
2011-09-12 16:03:21 +0800

    Scm/Git/Repo.js

--- Scm/Git/Repo.js
+++ Scm/Git/Repo.js
@@ -62,7 +62,7 @@ Repo = XObject.define(
                 }
                 line = line.substring(2);
                 
-                print(JSON.stringify(line));
+                //print(JSON.stringify(line));
                 var parts = line.split(/\s+/);
                 if (parts[1] == '->') {
                     return; // it's an alias like  remotes/origin/HEAD    -> origin/master
