df1aa538d7ce0374ab490753338086f566d757c3
Alan Knowles <alan@akbkhome.com>
2011-09-12 13:05:34 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -122,18 +122,30 @@ Merger=new XObject({
                                 //this.insert(citer,iter,0);
                                 this.model.el.clear();
                                 
+                                var master = false;
+                                var working = false;
+                                
                                 for(var i =0 ; i < tr.length; i++) {
                                     var ret = {  };
                                     this.model.el.append(ret);
                                     //print(JSON.stringify(ret,null,4));
-                                     
-                                      
+                                    if (tr[i].name == 'master') {
+                                        master = id;
+                                    }
+                                    if (tr[i].name == 'release') {
+                                        master = id;
+                                    }          
                                     this.model.el.set_value(ret.iter, 0, '' + tr[i].name );
                                     this.model.el.set_value(ret.iter, 1, '' + tr[i].rev  );
                              
                                     
                                 }     
-                                
+                                if (master !== false) {
+                                    this.el.set_active(master);
+                                }
+                                if (release !== false) {
+                                    this.el.set_active(release);
+                                }
                                 
                             },
                             model : {
