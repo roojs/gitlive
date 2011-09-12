cff58a3c9355e2f6811654291aa840087649645b
Alan Knowles <alan@akbkhome.com>
2011-09-12 10:45:49 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -248,7 +248,7 @@ Clones=new XObject({
                                     xtype: Gtk.TreeViewColumn,
                                     min_width : 50,
                                     pack : "append_column",
-                                    title : "Added",
+                                    title : "Current Branch",
                                     init : function() {
                                         XObject.prototype.init.call(this);
                                         this.el.add_attribute(this.items[0].el , 'markup', 1 );
