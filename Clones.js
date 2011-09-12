4f57ceaa4e18096b82b52c60dd033801b9244c80
Alan Knowles <alan@akbkhome.com>
2011-09-12 17:08:00 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -526,11 +526,6 @@ Clones=new XObject({
         },
         {
             xtype: Gtk.Button,
-            label : "Cancel",
-            pack : "add_action_widget,0"
-        },
-        {
-            xtype: Gtk.Button,
             id : "ok_button",
             label : "OK",
             pack : "add_action_widget,1"
