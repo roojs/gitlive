041a62e87c477ee9d4bbf240e8c3066f73ffb243
Alan Knowles <alan@akbkhome.com>
2011-09-12 11:47:53 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -113,7 +113,14 @@ Clones=new XObject({
                             xtype: Gtk.Button,
                             listeners : {
                                 clicked : function (self) {
+                                    
+                                    
+                                    
                                     var Merger =     imports.Merger.Merger;
+                                    
+                                    
+                                    
+                                    
                                     Merger.repo = new Repo('/home/alan/gitlive/roojs1');
                                     Merger.show();
                                 
