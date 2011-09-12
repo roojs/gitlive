ce7dddfa2e6b67a5b9865d848dcc634f03673d5b
Alan Knowles <alan@akbkhome.com>
2011-09-12 11:16:39 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -363,23 +363,6 @@ Clones=new XObject({
                                             pack : "pack_start"
                                         }
                                     ]
-                                },
-                                {
-                                    xtype: Gtk.TreeViewColumn,
-                                    min_width : 50,
-                                    pack : "append_column",
-                                    title : "Last updated",
-                                    init : function() {
-                                        XObject.prototype.init.call(this);
-                                        this.el.add_attribute(this.items[0].el , 'markup', 3 );
-                                    
-                                    },
-                                    items : [
-                                        {
-                                            xtype: Gtk.CellRendererText,
-                                            pack : "pack_start"
-                                        }
-                                    ]
                                 }
                             ]
                         },
