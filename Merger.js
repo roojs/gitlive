c295f2782cc05ec891daf199acb0fef584af9b69
Alan Knowles <alan@akbkhome.com>
2011-09-12 17:24:04 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -573,7 +573,7 @@ Merger=new XObject({
                                                     Merger.el.set_transient_for(Commit.el);
                                                 
                                                     Commit.show({
-                                                        files : this.get('/changedFilesStore').files()
+                                                        files : this.get('/changedFilesView').files()
                                                     
                                                     });
                                                 
