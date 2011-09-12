a08eb78f71d773fdcde24bd19b2553bf3fc843ff
Alan Knowles <alan@akbkhome.com>
2011-09-12 16:01:31 +0800

    Scm/Git/Repo.js

--- Scm/Git/Repo.js
+++ Scm/Git/Repo.js
@@ -554,7 +554,7 @@ Repo = XObject.define(
                 cwd : this.repopath,
                 args : [ 'patch' , '-p1' ] ,
                 env :  [  "HOME=" + GLib.get_home_dir() ],
-                debug: this.debug,
+                debug: false,
                 exceptions : false,
                 async : false,
                 listeners : {
@@ -695,7 +695,7 @@ Repo = XObject.define(
                 cwd : this.repopath,
                 args : args,
                 env : env, // optional
-                debug: false,
+                debug: this.debug,
                 exceptions : false,
                 async : false
             });
