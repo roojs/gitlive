64c724e3a41d39b903c1cf51fd93e7d9a02c0be3
Alan Knowles <alan@akbkhome.com>
2011-09-12 15:48:56 +0800

    test_git.js

--- test_git.js
+++ test_git.js
@@ -8,8 +8,9 @@ Gtk.init(Seed.argv);
 var repo = new Repo({ repopath : '/home/alan/gitlive/roojs1' });
  
 //var hist = repo.history('/', false, 'rev', 'github..master');
+var hist = repo.getBranches();
 
-var hist = repo.changedFiles('/',   'rev', 'github..master');
+//var hist = repo.changedFiles('/',   'rev', 'github..master');
 
 print(JSON.stringify(hist,null,4));
 /*
