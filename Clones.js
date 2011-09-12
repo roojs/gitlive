66afbfc770320989e691b3868092c1456d5c0c1d
Alan Knowles <alan@akbkhome.com>
2011-09-12 10:49:41 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -286,7 +286,7 @@ Clones=new XObject({
                                     title : "All Branches",
                                     init : function() {
                                         XObject.prototype.init.call(this);
-                                        this.el.add_attribute(this.items[0].el , 'markup', 1 );
+                                        this.el.add_attribute(this.items[0].el , 'markup', 2 );
                                     
                                     },
                                     items : [
