c4986376153b0704df69c1d58d929f60b18ed467
Alan Knowles <alan@akbkhome.com>
2011-09-12 11:19:21 +0800

    Clones.js

--- Clones.js
+++ Clones.js
@@ -228,7 +228,8 @@ Clones=new XObject({
                                           GObject.TYPE_STRING, // all branch      
                                           GObject.TYPE_STRING, // updated
                                            GObject.TYPE_BOOLEAN, // auto-commit
-                                            GObject.TYPE_BOOLEAN // auto-push
+                                            GObject.TYPE_BOOLEAN, // auto-push
+                                               GObject.TYPE_BOOLEAN // active
                                       ] );
                                     },
                                     load : function()
