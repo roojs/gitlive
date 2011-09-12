600354226e3ceeda8d11862f85bea9b15d24cbd9
Alan Knowles <alan@akbkhome.com>
2011-09-12 13:26:20 +0800

    Merger.js

--- Merger.js
+++ Merger.js
@@ -10,66 +10,20 @@ Vte = imports.gi.Vte;
 console = imports.console;
 XObject = imports.XObject.XObject;
 Merger=new XObject({
-    xtype: Gtk.Dialog,
+    xtype: Gtk.Button,
     listeners : {
-        destroy_event : function (self, event) {
-             this.el.hide();
-                        return false;
-        },
-        response : function (self, id) {
-          // hide
-             //if (id < 1) {
-                this.el.hide();
-                return;
-            //}
-            if (typeof(this.get('bug').getValue()) != 'object') {
-                print("ERROR");
-                return;
-            }
+        clicked : function (self) {
+            
+            
          
-            this.el.hide();
-                
-            //var val = this.get('bug').getValue();
-             //   Seed.print(val);
+            var hist = Merger.repo.dayTree('/', false, 'rev', 'github..master');
+            this.get('/historyTreeStore').load(hist);
+                 
+            
         }
     },
-    border_width : 3,
-    default_height : 700,
-    default_width : 800,
-    id : "Merger",
-    title : "Merger",
-    deletable : true,
-    show : function(c) {
-        
-        if (!this.el) {
-            this.init();
-        }
-        var _this = this;
-         
-          //this.el.set_title("Merger - ??? ");
-    //   this.el.set_title("Merger - " + this.repo.repopath);
-    
-    
-    
-         /// load up branches
-         this.get('/workingCombo').load(Merger.repo.branches);
-         
-         this.get('/releaseCombo').load(Merger.repo.branches);
-    
-        this.el.show_all();
-        //this.get('/ok_button').el.set_sensitive(false);
-        
-        // block until we return.
-        var run_ret = this.el.run();
-        if (run_ret < 1 ) {
-            return  "DONE";
-        }
-        print("RUN RETURN : " + run_ret);
-        return "DONE";
-        //print(JSON.stringify(this.get('bug').getValue()));
-       // return this.get('bug').getValue();
-        //this.success = c.success;
-    },
+    label : "Select Branch",
+    pack : "add",
     items : [
         {
             xtype: Gtk.VBox,
