4c716ff29f51688868b9465cfc36db195fc01d44
Alan Knowles <alan@akbkhome.com>
2011-09-18 22:03:12 +0800

    test_git.js

--- test_git.js
+++ test_git.js
@@ -14,22 +14,5 @@ var hist = repo.history('/', false, 'rev', 'github..master');
 
 
 print(JSON.stringify(hist,null,4));
- 
-/*
-
-var Git = imports.Git.Git;
-var git = new Git('/home/alan/gitlive/roojs1');
-git.async = true;
-var out = git.run(
-{ 'no-pager' : true },  
-   'log',
-   'github..master',
-   { 'no-color' : true },
-   { 'raw' : true },
-   { 'numstat' : true },
-   { 'date' : 'rfc' }  
-);
-print(out);
- 
-*/
-Gtk.main();
+  
+//Gtk.main();
