719009f62d604630883a8ecf66eb08ca410bd561
Alan Knowles <alan@akbkhome.com>
2011-09-18 20:40:14 +0800

    gitlive.js

--- gitlive.js
+++ gitlive.js
@@ -151,7 +151,7 @@ var monitor = new Monitor({
             });
             //repo.debug = 1;
             // these can fail... at present... as we wildcard stuff.
-            try {  repo.add(add_files); } catch(e) { }
+            repo.add(add_files); 
             repo.remove(remove_files);
             
             success.push(repo.commit({
