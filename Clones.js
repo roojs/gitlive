8494a87c9c6dae680c3f91da36b3f550f6dbbb05
Alan Knowles <alan@akbkhome.com>
2012-05-23 22:21:45 +0800

    Clones.bjs Clones.js

--- Clones.js
+++ Clones.js
@@ -472,6 +472,7 @@ Clones=new XObject({
                                     xtype: Gtk.TreeViewColumn,
                                     min_width : 200,
                                     pack : "append_column",
+                                    resizable : "true",
                                     title : "Repo",
                                     init : function() {
                                         XObject.prototype.init.call(this);
