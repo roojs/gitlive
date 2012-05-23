f203b56ce78f28e87d05cb8054052afa72f4e830
Alan Knowles <alan@akbkhome.com>
2012-05-23 23:23:07 +0800

    tests/soup.js

--- tests/soup.js
+++ tests/soup.js
@@ -25,8 +25,7 @@ netrc.split("\n").forEach(function(nl) {
    authdata[line.machine] = line;
     
 });
-print(JSON.stringify(lines));
-
+ 
 // Soup.URI is a struct.
 var uri = new Soup.URI.c_new("http://www.roojs.com/admin.php/Roo/person");
 var msg = new Soup.Message({method:"GET", uri:uri});
