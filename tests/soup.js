d2c81a763290176fba47a8549154908d633ec886
Alan Knowles <alan@akbkhome.com>
2012-05-23 22:50:42 +0800

    tests/soup.js

---
+++ tests/soup.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env seed
+
+Soup = imports.gi.Soup;
+
+var session = new Soup.SessionSync();
+
+// Soup.URI is a struct.
+var uri = new Soup.URI.c_new("http://www.roojs.com/admin.php");
+var msg = new Soup.Message({method:"GET", uri:uri});
+//request.headers_append...
+var status = session.send_message(msg);
+// session.queue_message(function(ses, msg) {
+//
+//}
+//)
+print(status);
+
+print(msg.response_body);
