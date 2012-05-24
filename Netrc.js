c03e0a9e78e82ebe3b5f677c366a534cacf83fb7
Alan Knowles <alan@akbkhome.com>
2012-05-25 06:17:12 +0800

    Netrc.js

---
+++ Netrc.js
@@ -0,0 +1,39 @@
+const GLib = imports.gi.GLib;
+
+const File  = imports['../File.js'].File;
+
+
+Netrc = {
+    
+    var data = {};
+    
+    load : function()
+    {
+        
+        var netrc = File.read(GLib.get_home_dir() + "/.netrc");
+        var authdata = {};
+        netrc.split("\n").forEach(function(nl) {
+            var line = {};
+            var k = false
+             
+            nl.replace(/\s+$/,'').replace(/^\s+/,'').split(/\s+/).forEach(function(kv) {
+              
+                if (!k) {
+                    k = kv;
+                    return
+                }
+                line[k] = kv
+                k = false;
+           });
+               
+           authdata[line.machine] = line;
+            
+        });
+        this.data = authdata;
+         
+    }
+    
+    forhost: function
+    
+    
+}
