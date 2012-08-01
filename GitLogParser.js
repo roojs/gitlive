66fb1481229bb73254bfbd57a201f35e5a5a717c
Alan Knowles <alan@akbkhome.com>
2012-08-02 07:46:56 +0800

    GitLogParser.js

--- GitLogParser.js
+++ GitLogParser.js
@@ -146,7 +146,7 @@ for (var h in res) {
     for (var p in res[h]) {
         switch(p) {
             case 'IDLE':
-                print(h + ' ' + res[h].total/6000 +'m' + )
+                print(h + ' ' + res[h].total/60000 +'m' + )
         }
         
         
