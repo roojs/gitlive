e260d603eb7eb0e2e3f3a0ffad6c5cbb9e0c3970
Alan Knowles <alan@akbkhome.com>
2012-11-18 13:07:57 +0800

    Browser/Settings.js

--- Browser/Settings.js
+++ Browser/Settings.js
@@ -1,11 +1,8 @@
 
 Netrc = imports['../Netrc.js'].Netrc;
-var cfg = Netrc.forHost('git.roojs.com');
-var home_page = 'http://' +  encodeURIComponent(cfg.login) +
-	':' + encodeURIComponent(cfg.password) + '@www.roojs.com/admin.php';
-print(home_page);
-print(JSON.stringify(cfg,null,4));
- 
+var auth_data=  = Netrc.forHost('git.roojs.com');
+var home_page = 'www.roojs.com/admin.php';
+  
 var select_new_tabs = false;
 
 // Test for Gtk >= 2.16 (otherwise don't have a progress bar)
