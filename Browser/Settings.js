b837591d43ac7c4f26b6f95f0cb732bf8aff6690
Alan Knowles <alan@akbkhome.com>
2012-11-18 12:55:39 +0800

    Browser/Settings.js

--- Browser/Settings.js
+++ Browser/Settings.js
@@ -1,7 +1,8 @@
 
 Netrc = imports['../Netrc.js'].Netrc;
 var cfg = Netrc.forHost('git.roojs.com');
-var home_page = 'http://' +  cfg.login + ':' + cfg.password + '@www.roojs.com/admin.php';
+var home_page = 'http://' +  encodeURIComponent(cfg.login) +
+	':' + encodeURIComponent(cfg.password) + '@www.roojs.com/admin.php';
  
  
 var select_new_tabs = false;
