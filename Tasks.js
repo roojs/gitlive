86d0579aaa26ddacdba2ed94bc6df589ec511a37
Alan Knowles <alan@roojs.com>
2012-11-25 02:24:45 +0800

    Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -52,7 +52,7 @@ Tasks = {
                 }
                 var res = JSON.parse(this.responseText);
                 
-                
+                JSON.stringify(res,null,4)
                 _this.curTask = (res.success && res.data.length) ? new Task(res.data[0]) : false;
                 print(JSON.stringify(_this.curTask,null,4));
                 _this.verifyCommit();
