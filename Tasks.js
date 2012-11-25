934451f135ae32aa123aaef08f6df7029ebcbd0d
Alan Knowles <alan@akbkhome.com>
2012-11-25 20:34:53 +0800

    Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -72,7 +72,8 @@ Tasks = {
                 //print(JSON.stringify(res,null,4))
                 //print([ res.success , res.data.length ]);
                 _this.curTask = (res.success && res.data.length) ? (new Task(res.data[0])) : false;
-                print(JSON.stringify(_this.curTask,null,4));
+                
+                print("Current task:" + JSON.stringify(_this.curTask,null,4));
                 _this.fetchRepo();
             }
             
