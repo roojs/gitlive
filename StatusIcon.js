01420a9bad0b1ef1fbe5bf3661b1806a009cce43
Alan Knowles <alan@akbkhome.com>
2012-11-25 17:53:25 +0800

    StatusIcon.js

--- StatusIcon.js
+++ StatusIcon.js
@@ -161,6 +161,7 @@ var StatusIcon  = new XObject({
                                 }
                             }
                             
+                            
                              
                           
                             imports.GitMonitor.GitMonitor.start();
