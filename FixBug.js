eb2af0cfe945ba11fa1127fd38377981ae9b25a8
Alan Knowles <alan@roojs.com>
2012-12-09 22:48:25 +0800

    FixBug.bjs FixBug.js

--- FixBug.js
+++ FixBug.js
@@ -64,7 +64,7 @@ FixBug=new XObject({
             items : [
                 {
                     xtype: Gtk.HBox,
-                    pack : "pack_end,false,true,3",
+                    pack : "pack_start,false,true,3",
                     items : [
                         {
                             xtype: Gtk.Label,
