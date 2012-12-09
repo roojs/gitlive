e7f9ad08e8e4c70c70cbbd4a94f52707b11ae00e
Alan Knowles <alan@roojs.com>
2012-12-09 18:41:20 +0800

    XObject.js

--- XObject.js
+++ XObject.js
@@ -201,7 +201,7 @@ XObject.prototype = {
             try {
                 this.el = new (this.xtype)(this.config);
             } catch(e) {
-                print(JSON.stringify(e, null,4));
+                print(JSON.stringify(e.stack, null,4));
                  throw {
                     name: "ArgumentError", 
                     message :"Error creating object from xtype(object)"
