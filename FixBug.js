2f24d21ab78c720c65f232ae13af12617a39ef6a
Alan Knowles <alan@roojs.com>
2012-12-09 22:09:07 +0800

    FixBug.bjs FixBug.js

--- FixBug.js
+++ FixBug.js
@@ -327,7 +327,8 @@ FixBug=new XObject({
                                             }
                                         },
                                         {
-                                            xtype: Gtk.TreeViewColumn
+                                            xtype: Gtk.TreeViewColumn,
+                                            pack : "append_column"
                                         }
                                     ]
                                 }
