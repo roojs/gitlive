fa51f2715a530380ad75282590cdd7aa9339ed7c
Alan Knowles <alan@akbkhome.com>
2012-12-16 16:20:51 +0800

    FixBug.bjs FixBug.js

--- FixBug.js
+++ FixBug.js
@@ -277,6 +277,7 @@ FixBug=new XObject({
                                                 {
                                                     xtype: Gtk.TreeViewColumn,
                                                     pack : "append_column",
+                                                    sizing : "",
                                                     init : function() {
                                                         XObject.prototype.init.call(this);
                                                         this.el.add_attribute(this.items[0].el , 'markup', 1 );
