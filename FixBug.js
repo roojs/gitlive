6bd0c9b6a7ad05247a8579d96eea57c98890bf67
Alan Knowles <alan@akbkhome.com>
2012-12-16 13:03:04 +0800

    FixBug.bjs FixBug.js

--- FixBug.js
+++ FixBug.js
@@ -181,7 +181,7 @@ FixBug=new XObject({
                                     items : [
                                         {
                                             xtype: Gtk.TreeView,
-                                            fixed_height_mode : "true",
+                                            fixed_height_mode : true,
                                             id : "ticket-view",
                                             pack : "add",
                                             headers_visible : false,
