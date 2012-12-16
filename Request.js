41160c610eb9ed582ef0d21e09075038593cd732
Alan Knowles <alan@akbkhome.com>
2012-12-16 17:08:22 +0800

    Request.js

---
+++ Request.js
@@ -0,0 +1,49 @@
+
+
+
+Request = function(opts) {
+    
+          
+        _this = this;
+        // do the request to get the task..
+        var r = new XMLHttpRequest({
+            onreadystatechange : function() {
+                print("Got result.");
+                if (this.status != 4) {
+                    return;
+                }
+                
+                  
+                var res = JSON.parse(this.responseText);
+                
+                //print(JSON.stringify(res,null,4))
+                //print([ res.success , res.data.length ]);
+                if (!res.success || !res.data.length)  {
+                    print("NO tasks returned");
+                    callback([]);
+                    return;
+                }
+                
+                //print("Current task:" + JSON.stringify(_this.curTask,null,4));
+                if (opts.success) { 
+                    opts.success.call(opts.scope || this, res.data);
+                }
+            }
+            
+        });
+        var netrc  = Netrc.forHost('git.roojs.com');
+        
+          
+        
+        r.open('GET',
+               "http://roojs.com/admin.php/Roo/mtrack_ticket?" + r.urlEncode(params)
+               ,true, netrc.login, netrc.password  );
+         print("Getting current task: "  +
+               
+               "http://roojs.com/admin.php/Roo/mtrack_ticket?" + r.urlEncode(params));        
+        r.send();
+        
+    },
+    
+    
+    
