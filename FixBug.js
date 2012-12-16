40a7f51cc6f5460b465530d5f0851421e42791bb
Alan Knowles <alan@akbkhome.com>
2012-12-16 16:46:15 +0800

    FixBug.bjs FixBug.js

--- FixBug.js
+++ FixBug.js
@@ -450,7 +450,37 @@ FixBug=new XObject({
                             items : [
                                 {
                                     xtype: Gtk.HBox,
-                                    pack : "pack_start,false,true,3"
+                                    pack : "pack_start,false,true,3",
+                                    items : [
+                                        {
+                                            xtype: Gtk.Button,
+                                            listeners : {
+                                                clicked : function (self) {
+                                                    var active_id = this.get('/project-select').el.get_active();
+                                                     
+                                                    var project_id = this.get('/project-select').raw_data[active_id].id;
+                                                    
+                                                    var str = this.get('/search-entry').el.get_text();
+                                                    
+                                                    print(str);
+                                                    
+                                                    var _t = this;
+                                                    
+                                                    imports.Tasks.Tasks.query({
+                                                        project_id : project_id,
+                                                        'query[filter]' : 'me',
+                                                        'query[search]' : str
+                                                    }, function(res) { 
+                                                       // print(JSON.stringify(res,null,4));
+                                                        _t.get('/ticket-store').loadData(res);
+                                                    });
+                                                }
+                                            },
+                                            label : "Search",
+                                            pack : "pack_end,false,true,3",
+                                            use_stock : true
+                                        }
+                                    ]
                                 },
                                 {
                                     xtype: Gtk.ScrolledWindow,
