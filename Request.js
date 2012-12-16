38536086df42734f599bd94bd3cfc96cb016f2d8
Alan Knowles <alan@akbkhome.com>
2012-12-16 17:11:14 +0800

    Request.js

--- Request.js
+++ Request.js
@@ -2,53 +2,53 @@
 
 
 Request = function(opts) {
-    
-          
-        _this = this;
-        // do the request to get the task..
-        var r = new XMLHttpRequest({
-            
-            onreadystatechange : function()
-            {
-                print("Got result.");
-                if (this.status != 4) {
-                    return;
-                }
-                
-                  
-                var res = JSON.parse(this.responseText);
-                
-                //print(JSON.stringify(res,null,4))
-                //print([ res.success , res.data.length ]);
-                if (!res.success || !res.data.length)  {
-                    print("NO tasks returned");
-                    callback([]);
-                    return;
-                }
-                
-                //print("Current task:" + JSON.stringify(_this.curTask,null,4));
-                if (opts.success) { 
-                    opts.success.call(opts.scope || this, res.data);
-                }
+
+      
+    _this = this;
+    // do the request to get the task..
+    var r = new XMLHttpRequest({
+        
+        onreadystatechange : function()
+        {
+            print("Got result.");
+            if (this.status != 4) {
+                return;
             }
             
-        });
+              
+            var res = JSON.parse(this.responseText);
+            
+            //print(JSON.stringify(res,null,4))
+            //print([ res.success , res.data.length ]);
+            if (!res.success || !res.data.length)  {
+                print("NO tasks returned");
+                callback([]);
+                return;
+            }
+            
+            //print("Current task:" + JSON.stringify(_this.curTask,null,4));
+            if (opts.success) { 
+                opts.success.call(opts.scope || this, res.data);
+            }
+        }
         
+    });
+    
+    
+    var netrc  = Netrc.forHost('git.roojs.com');
+    
+      
+    
+    r.open('GET',
+           'http://roojs.com/admin.php' + opts.url + '?' + r.urlEncode(opts.params)
+           ,true, netrc.login, netrc.password  );
+    print("Getting current task: "  +
+            'http://roojs.com/admin.php' + opts.url + '?' + r.urlEncode(opts.params)
+         );        
+    r.send();
         
-        var netrc  = Netrc.forHost('git.roojs.com');
-        
-          
-        
-        r.open('GET',
-               'http://roojs.com/admin.php' + opts.url + '?' + r.urlEncode(opts.params)
-               ,true, netrc.login, netrc.password  );
-        print("Getting current task: "  +
-                'http://roojs.com/admin.php' + opts.url + '?' + r.urlEncode(opts.params)
-             );        
-        r.send();
-        
-    }
 }
+
     
     
     
