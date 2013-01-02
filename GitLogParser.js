dbe35f335a89ad7ffa7c7ae8fda62f55a9fedf35
Alan Knowles <alan@roojs.com>
2013-01-02 14:54:16 +0800

    GitLogParser.js

--- GitLogParser.js
+++ GitLogParser.js
@@ -225,11 +225,13 @@ for (var h in res) {
 }
 for (var h in GitLogParser.shours) {
     var hsum = [];
+    var htot = 0;
     for (var desc in GitLogParser.shours[h]) {
+        htot += GitLogParser.shours[h][desc]/60000).toFixed(2)*1;
         hsum.push({ desc : desc, tot : (GitLogParser.shours[h][desc]/60000).toFixed(2)*1 })
     }
     hsum.sort(function(a,b) { if (a.tot == b.tot) { return 0; } return a.tot < b.tot ? 1 : -1 });
-    print(h+':');
+    print(h+': Total (' + htot +')');
     hsum.forEach(function(r) {
         print ("  " + r.tot + "   : " + r.desc);
     });
